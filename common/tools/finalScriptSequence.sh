1e345efd6bee3bbc6ff984ec6edc124ecd14a414
harman-f <98130152+harman-f@users.noreply.github.com>
2022-07-09 02:13:05 +0200

    updated for new apps

--- common/tools/finalScriptSequence.sh
+++ common/tools/finalScriptSequence.sh
@@ -2,15 +2,23 @@
 
 # change this file to run any command, script or binary from SD
 # no need to touch metainfo2.txt as this is outside of its reach
-# final script  run time is set to 30s in metainfo2.txt
+# final script  run time is set to 300s in metainfo2.txt
 # if you need more time extend FinalScriptMaxTime = XX
 # add echo to output debugging information to RCC log on UART
 
 echo "[finalScriptSequence] Cool stuff found and executing..."
 
 export GEM=1
+export SWDLAUTORUN=1 # allows Swdlautorun.txt in root of SD during SWDL process
 
-/net/mmx/fs/sda0/apps/aio -all
+echo "Runnung basic backup --------------------"
+${2}/apps/backup -b
+echo "Runnung AIO generic script --------------"
+${2}/apps/aio_generic -all
+#echo "Runnung AIO special script -------------"
+#${2}/apps/aio_special -all
+echo "Runnung POG11 button patch script --------------"
+${2}/apps/addimage -pog11on -noboot
 
 echo "[finalScriptSequence] Cool stuff DONE"
 
